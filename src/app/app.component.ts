89d32141cfc491697a4c90dbd7eae01dafd8251f
MrF <fayazbasha974@gmail.com>
2017-06-09 21:48:47 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   openCheckout() {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_DTZyElWfcyAZGhm461wfNk1e',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+      }
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: 2000
+    });
+
+  }
 }
